11c9fddca92b94108dea438c16f877ac96556e84
ylang <ylang@pku.edu.cn>
2021-05-22 15:30:52 +0800

    n_up_to < 0

--- code/lowpass_n.m
+++ code/lowpass_n.m
@@ -4,8 +4,9 @@ global n_up_to
 
 persistent f_n
 
-f_n = fft(f(:, :, 2:end-1), [], 3);
-f_n(:, :, n_up_to+2: end-n_up_to) = 0;
-f(:, :, 2:end-1) = ifft(f_n, [], 3);
-f = zbcs(f);
-
+if n_up_to >= 0
+	f_n = fft(f(:, :, 2:end-1), [], 3);
+	f_n(:, :, n_up_to+2: end-n_up_to) = 0;
+	f(:, :, 2:end-1) = ifft(f_n, [], 3);
+	f = zbcs(f);
+end
